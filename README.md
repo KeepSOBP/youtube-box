79227a6b73a95ec379bb8433ac8938f4d4203b3c
KeepSOBP <keepsobp@naver.com>
2020-10-07 13:17:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![](https://cdn.statically.io/img/bread.shx.gg/f=auto,w=450,h=450/o6HKYm.png)
+
+# youtube-box
+
+**Update Every 5mins**
+
+[Original git](https://github.com/extremecodetv/youtube-box)
+
+# How to use
+1. Create a new public GitHub Gist (https://gist.github.com/), create files named `Last Video On Youtube.md`
+
+2. Create an access token with the gist scope and copy it. (https://github.com/settings/tokens/new)
+
+3. Get a YouTube API key - follow these [steps](https://developers.google.com/youtube/v3/getting-started) note that you don't need an Oauth scope for this.
+
+4. [Click me and make repo](https://github.com/keepsobp/youtube-box/generate)
+
+5. Edit the environment variables in `.github/workflows/main.yml`:
+
+   - **CHANNEL_ID**: The ID portion of your YouTube channel url: `https://www.youtube.com/channel/`**`UCl7UlXHLVg2hs7VbrZs0y3Q`**
+
+   - **GIST_ID**: The ID portion of your gist url: `https://gist.github.com/KeepSOBP/`**`a1b1dd01baa4f1342b05fbde5f80f782`**
+
+6. Go to the repo `Settings` > `Secrets`
+
+7. Add the following environment variables:
+
+  - **GH**: The GitHub access token generated above.
+  - **YAK**: The YouTube API key generated above.
---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+
+const { Octokit } = require('@octokit/rest')
+const Axios = require('axios')
+const { stripIndents } = require('common-tags')
+
+const {
+  GIST_ID: gistId,
+  GH_TOKEN: githubToken,
+  YOUTUBE_API_KEY: youtubeApiKey,
+  YOUTUBE_CHANNEL_ID: youtubeChannelId
+} = process.env
+
+const octokit = new Octokit({ auth: `token ${githubToken}` })
+
+const getLastVideo = async => Axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${youtubeChannelId}&maxResults=1&order=date&type=video&key=${youtubeApiKey}`)
+
+const updateGist = async ({ box }) => {
+  let gist
+  try {
+    gist = await octokit.gists.get({ gist_id: gistId })
+  } catch (error) {
+    console.error(`Unable to get gist\n${error}`)
+  }
+
+  const filename = Object.keys(gist.data.files)[0]
+  await octokit.gists.update({
+    gist_id: gistId,
+    files: {
+      [filename]: {
+        filename: 'Last Video On Youtube.md',
+        content: box
+      }
+    }
+  })
+}
+
+const main = async () => {
+  const { data: respone } = await getLastVideo()
+  const video = respone.items.shift()
+
+  const box = stripIndents`
+    ## Recent uploaded video
+
+    ### ${video.snippet.title}
+
+    [![thumbnail](http://img.youtube.com/vi/${video.id.videoId}/0.jpg)](https://youtu.be/${video.id.videoId})
+  `
+
+  await updateGist({ box })
+}
+
+(async () => {
+  await main()
+})()
